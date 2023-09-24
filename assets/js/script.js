190b8b1a4989ce8a3d0899e1a995cbe8ed6fb1fd
Dipika Shrestha <dipikashrestha@Dipikas-MacBook-Pro.local>
2023-09-24 01:07:41 -0500

    Added generate password logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,8 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
 function generatePassword() {
-  var generatedPassword;
   var passwordLength;
-
   var invalidPasswordLenth = true;
 
   // repeat until valid password length is provided
@@ -22,47 +19,45 @@ function generatePassword() {
       "Password Length - Enter the length of password between at least 8 characters and no more than 128 characters:"
     );
 
+    // exit when cancel is pressed in prompt dialog
     if (passwordLength == null) {
       return;
     }
-    
-    // user input validation 
-    if (
-      typeof passwordLength == "string" ||
-      passwordLength < 8 ||
-      passwordLength > 128
-    ) {
+
+    // user input validation
+    if (passwordLength < 8 || passwordLength > 128) {
       alert("Error: Please enter a valid number between 8 to 128.");
     } else {
       invalidPasswordLenth = false;
     }
   }
 
-  var lowercase;
-  var uppercase;
-  var numeric;
-  var specialChar;
+  var addLowercase;
+  var addUppercase;
+  var addNumeric;
+  var addSpecialChar;
   var validCharType;
 
   var invalidCharType = true;
-  
+
   // repeat until atleast one character type is selected
   while (invalidCharType) {
-    lowercase = confirm(
+    addLowercase = confirm(
       "Character Type - Do you want to include Lowercase characters?"
     );
-    uppercase = confirm(
+    addUppercase = confirm(
       "Character Type - Do you want to include Uppercase characters?"
     );
-    numeric = confirm(
+    addNumeric = confirm(
       "Character Type - Do you want to include Numeric characters?"
     );
-    specialChar = confirm(
+    addSpecialChar = confirm(
       "Character Type - Do you want to include Special characters?"
     );
 
-    // user input validation 
-    validCharType = lowercase || uppercase || numeric || specialChar;
+    // user input validation
+    validCharType =
+      addLowercase || addUppercase || addNumeric || addSpecialChar;
 
     if (validCharType) {
       invalidCharType = false;
@@ -71,10 +66,44 @@ function generatePassword() {
     }
   }
 
-  // TODO write logic to build the password
+  var generatedPassword = "";
+  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var lowerChars = "abcdefghijklmnopqrstuvwxyz".split("");
+  var numericChars = "0123456789".split("");
+  var specialChars = ["!","@","#","$","%","^","&","*","(",")","-","_","+","=","<",">","?","|","[","]","{","}",";",":",",",".","~",
+  ];
 
+  var selectedChars = [];
+
+  // check character type user selection
+  if (addLowercase) {
+    selectedChars = selectedChars.concat(lowerChars);
+  }
+
+  if (addUppercase) {
+    selectedChars = selectedChars.concat(upperChars);
+  }
+
+  if (addNumeric) {
+    selectedChars = selectedChars.concat(numericChars);
+  }
+
+  if (addSpecialChar) {
+    selectedChars = selectedChars.concat(specialChars);
+  }
+
+  // generate random password of user selected length
+  for (var i = 0; i < passwordLength; i++) {
+    generatedPassword = generatedPassword.concat(
+      selectedChars[getRandomIndex(selectedChars.length - 1)]
+    );
+  }
 
   return generatedPassword;
+}
+
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max); // Generates a random number between 0 and max
 }
 
 // Add event listener to generate button
